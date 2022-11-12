5930f07defebf0d7efd8a77c1d7330ac1f6fdf23
Justin Liu <justinwl2097@gmail.com>
2022-11-12 13:59:03 -0600

    removed console.logs

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,15 +3,12 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-  console.log('beforeinstallprompt');
   window.deferredPrompt = event;
   butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-  console.log('butInstall clicked');
-
   const promptEvent = window.deferredPrompt;
 
   if (!promptEvent) {
@@ -28,4 +25,5 @@ butInstall.addEventListener('click', async () => {
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   window.deferredPrompt = null;
+  butInstall.textContent = "Installed!";
 });
