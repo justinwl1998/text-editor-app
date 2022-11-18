655bd23fa8be584beb39341e7822f4bb7c404c14
Justin Liu <justinwl2097@gmail.com>
2022-11-17 18:17:15 -0600

    putDb now uses put instead of add

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({ text: content });
+  const request = store.put({ text: content });
   const result = await request;
   console.log('putDb successfully updated: ', result);
 }
@@ -32,6 +32,7 @@ export const getDb = async () => {
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
+  return result.value;
 }
 
 initdb();
